ae1e94ba4ce64b418278492a648a2a1612ee80fb
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-06-11 17:37:03 +0900

    abc288

---
+++ abc288/c.py
@@ -0,0 +1,177 @@
+# Atcoder ABC288-C Don’t be cycle
+# Union Findを使った解答例
+
+from collections import defaultdict
+import io
+import sys
+
+_INPUT = """\
+5 3
+1 2
+1 3
+2 3
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+
+class UnionFind():
+    """
+    Union Find木クラス
+
+    Attributes
+    --------------------
+    n : int
+        要素数
+    root : list
+        木の要素数
+        0未満であればそのノードが根であり、添字の値が要素数
+    rank : list
+        木の深さ
+    """
+
+    def __init__(self, n):
+        """
+        Parameters
+        ---------------------
+        n : int
+            要素数
+        """
+        self.n = n
+        self.root = [-1]*(n+1)
+        self.rank = [0]*(n+1)
+
+    def find(self, x):
+        """
+        ノードxの根を見つける
+
+        Parameters
+        ---------------------
+        x : int
+            見つけるノード
+
+        Returns
+        ---------------------
+        root : int
+            根のノード
+        """
+        if (self.root[x] < 0):
+            return x
+        else:
+            self.root[x] = self.find(self.root[x])
+            return self.root[x]
+
+    def unite(self, x, y):
+        """
+        木の併合
+
+        Parameters
+        ---------------------
+        x : int
+            併合したノード
+        y : int
+            併合したノード
+        """
+        x = self.find(x)
+        y = self.find(y)
+
+        if (x == y):
+            return
+        elif (self.rank[x] > self.rank[y]):
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if (self.rank[x] == self.rank[y]):
+                self.rank[y] += 1
+
+    def same(self, x, y):
+        """
+        同じグループに属するか判定
+
+        Parameters
+        ---------------------
+        x : int
+            判定したノード
+        y : int
+            判定したノード
+
+        Returns
+        ---------------------
+        ans : bool
+            同じグループに属しているか
+        """
+        return self.find(x) == self.find(y)
+
+    def size(self, x):
+        """
+        木のサイズを計算
+
+        Parameters
+        ---------------------
+        x : int
+            計算したい木のノード
+
+        Returns
+        ---------------------
+        size : int
+            木のサイズ
+        """
+        return -self.root[self.find(x)]
+
+    def roots(self):
+        """
+        根のノードを取得
+
+        Returns
+        ---------------------
+        roots : list
+            根のノード
+        """
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def group_size(self):
+        """
+        グループ数を取得
+
+        Returns
+        ---------------------
+        size : int
+            グループ数
+        """
+        return len(self.roots())
+
+    def group_members(self):
+        """
+        全てのグループごとのノードを取得
+
+        Returns
+        ---------------------
+        group_members : defaultdict
+            根をキーとしたノードのリスト
+        """
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+
+N, M = map(int, input().split())
+E = []
+for _ in range(M):
+    E.append(list(map(int, input().split())))
+# else:
+#     print(E)
+
+uf = UnionFind(N)
+
+cnt = 0
+for a, b in E:
+    if uf.find(a) != uf.find(b):
+        uf.unite(a, b)
+    else:
+        # 既に同じグループにいる場合は線を繋げない（閉路になってしまう）ため、
+        # 削除する辺としてカウントする。
+        cnt += 1
+
+print(cnt)
