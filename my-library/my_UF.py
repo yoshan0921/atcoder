967e80468df78112aa77e48ac19e5da4ca94c847
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-08-27 17:03:30 +0900

    modified my library

--- my-library/my_UF.py
+++ my-library/my_UF.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 
-class UnionFind():
+class UnionFind_v1():
     def __init__(self, n):
         self.n = n
         self.parents = [-1] * n
@@ -50,6 +50,46 @@ class UnionFind():
 
     def __str__(self):
         return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+
+class UnionFind_v2():
+    # 初期化
+    def __init__(self, n):
+        self.par = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
+
+    # 根を求める
+    def root(self, x):
+        if self.par[x] == -1:
+            return x  # x が根の場合は x を返す
+        else:
+            self.par[x] = self.root(self.par[x])  # 経路圧縮
+            return self.par[x]
+
+    # x と y が同じグループに属するか (根が一致するか)
+    def issame(self, x, y):
+        return self.root(x) == self.root(y)
+
+    # x を含むグループと y を含むグループを併合する
+    def unite(self, x, y):
+        # x 側と y 側の根を取得する
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:
+            return False  # すでに同じグループのときは何もしない
+        # union by rank
+        if self.rank[rx] < self.rank[ry]:  # ry 側の rank が小さくなるようにする
+            rx, ry = ry, rx
+        self.par[ry] = rx  # ry を rx の子とする
+        if self.rank[rx] == self.rank[ry]:  # rx 側の rank を調整する
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry]  # rx 側の siz を調整する
+        return True
+
+    # x を含む根付き木のサイズを求める
+    def size(self, x):
+        return self.siz[self.root(x)]
 
 
 if __name__ == "__main__":
